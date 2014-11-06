fbb1559858796c3a18c36898999ecfda498a91e4
PedroVFX <pedrovpatrao@gmail.com>
2014-11-06 21:10:18 +0000

    inline render

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,7 @@ class WelcomeController < ApplicationController
   # GET /welcome
   def index
 	respond_to do |format| 
-		format.js {render inline: "location.reload();" } 
+		format.js {render inline: "location = '#{root_url}';"} 
 	end
   end
 
