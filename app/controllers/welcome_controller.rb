2fcbd8521bcafb5c81b831e3b736cdfde1e724e9
PedroVFX <pedrovpatrao@gmail.com>
2014-11-06 23:20:37 +0000

    voltar sem turbolinks

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,9 +2,7 @@ class WelcomeController < ApplicationController
 
   # GET /welcome
   def index
-	respond_to do |format| 
-		format.js {render inline: "location = '#{root_url}';"} 
-	end
+
   end
 
 end
